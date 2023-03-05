764cc8a57cb38cf8de3aa217642f945636f455b7
Pedro Tozzi <pedrogbtozzi@gmail.com>
2023-03-04 23:00:44 -0300

    Implementando layout, componente Header alterando estrutura de pastas

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { HeaderContainer } from "./styles";
+import Logo from "../../assets/logo.svg";
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+export function Header() {
+    return (
+        <HeaderContainer>
+            <img src={Logo} alt="Logo" />
+            <nav>
+                <NavLink to="/" title="Timer">
+                    <Timer size={24} />
+                </NavLink>
+                <NavLink to="/history" title="History">
+                    <Scroll size={24} />
+                </NavLink>
+            </nav>
+        </HeaderContainer>
+    )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,7 +20,6 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${(props) => props.theme.colors["green-500"]};
     }
 
 `;
