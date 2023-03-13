4bff4839ba509761afe19432a8c7aee050e8e368
Pedro Tozzi <pedrogbtozzi@gmail.com>
2023-03-13 13:32:36 -0300

    implementando form com react-hook-form e validacao com zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,6 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod"
 import { Play } from "phosphor-react";
 import {
     CountDownContainer,
@@ -9,10 +12,34 @@ import {
     TaskTimeInput
 } from "./styles";
 
+const newCycleSchema = z.object({
+    task: z.string({
+        required_error: "Task is required",
+        invalid_type_error: "Task must be between 1 and 50 characters",
+    }).min(1).max(50),
+    time: z.number().min(5).max(90).step(5)
+})
+type NewCycle = z.infer<typeof newCycleSchema>
+
 export function Home() {
+    const { register, handleSubmit, watch, reset } = useForm<NewCycle>({
+        resolver: zodResolver(newCycleSchema),
+        defaultValues: {
+            task: "",
+            time: 0
+        }
+    });
+
+    const onSubmit = (data: NewCycle) => {
+        console.log(data)
+        reset()
+    }
+
+    const isSubmitDisabled = !watch("task")
+
     return (
         <HomeContainer>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <FormContainer>
                     <label htmlFor="">Working on</label>
                     <TaskInput
@@ -20,6 +47,7 @@ export function Home() {
                         id="task"
                         placeholder="Give a name to your task"
                         list="tasks"
+                        {...register("task")}
                     />
                     <datalist id="tasks">
                         <option value="Study" />
@@ -31,12 +59,13 @@ export function Home() {
 
                     <label htmlFor="">during</label>
                     <TaskTimeInput
-                        type="text"
+                        type="number"
                         id="time"
                         placeholder="00"
                         step={5}
                         min={5}
                         max={99}
+                        {...register("time", { valueAsNumber: true })}
                     />
 
                     <span>minutes</span>
@@ -50,7 +79,7 @@ export function Home() {
                     <span>0</span>
                 </CountDownContainer>
 
-                <StartCountdownButton type="submit">
+                <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
                     <Play />
                     Start
                 </StartCountdownButton>
