35552e3ff6404dcb82042745034347aaa8596124
Pedro Tozzi <pedrogbtozzi@gmail.com>
2023-03-15 00:02:23 -0300

    Substituindo states de cycles e currentCycle por um reducer

--- src/Contexts/CycleContext.tsx
+++ src/Contexts/CycleContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode, useState } from "react";
+import {
+    createContext,
+    ReactNode,
+    useReducer,
+    useState
+} from "react";
 import { Cycle } from "../schemas/Cycle";
 import { NewCycle } from "../schemas/NewCycle";
 
@@ -9,65 +14,87 @@ interface CycleContext {
     onSubmit: (data: NewCycle) => void;
     handleStopCountdown: () => void;
 }
+export const CyclesContext = createContext({} as CycleContext)
 
 interface CyclesProviderProps {
     children: ReactNode;
 }
 
-export const CyclesContext = createContext({} as CycleContext)
+interface CycleStates {
+    cycles: Cycle[];
+    currentCycle: Cycle | null;
+}
+
 
 export function CyclesProvider({ children }: CyclesProviderProps) {
-    const [cycles, setCycles] = useState<Cycle[]>([])
-    const [currentCycle, setCurrentCycle] = useState<Cycle | null>(null)
-
-    const done = () => {
-        setCycles(state =>
-            state.map(cycle => {
-                if (cycle.id === currentCycle?.id) {
-                    return {
-                        ...cycle,
-                        done: true,
-                    }
+    const [cycleStates, dispatch] = useReducer((state: CycleStates, action: any) => {
+        switch (action.type) {
+            case "ADD":
+                return {
+                    ...state,
+                    cycles: [...state.cycles, action.payload],
+                    currentCycle: action.payload
                 }
-                return cycle
-            })
-        )
-        setCurrentCycle(null)
-    }
+            case "STOP":
+                return {
+                    ...state,
+                    cycles: state.cycles.map(cycle => {
+                        if (cycle.id === state.currentCycle?.id) {
+                            return {
+                                ...cycle,
+                                stopDate: new Date()
+                            }
+                        }
+                        return cycle
+                    }),
+                    currentCycle: null
+                }
+            case "DONE":
+                return {
+                    ...state,
+                    cycles: state.cycles.map(cycle => {
+                        if (cycle.id === state.currentCycle?.id) {
+                            return {
+                                ...cycle,
+                                done: true
+                            }
+                        }
+                        return cycle
+                    }),
+                    currentCycle: null
+                }
+            default:
+                return state
+        }
+    }, {
+        cycles: [],
+        currentCycle: null
+    })
 
     const onSubmit = (data: NewCycle) => {
-        const newCycle: Cycle = {
-            id: String(new Date().getTime()),
-            task: data.task,
-            time: data.time,
-            startDate: new Date(),
-            done: false
-        }
-        setCycles(state => [...state, newCycle])
-        setCurrentCycle(newCycle)
-        // reset()
+        dispatch({
+            type: "ADD",
+            payload: {
+                id: String(new Date().getTime()),
+                task: data.task,
+                time: data.time,
+                startDate: new Date(),
+                done: false
+            }
+        })
     }
 
     const handleStopCountdown = () => {
-        setCycles(state =>
-            state.map(cycle => {
-                if (cycle.id === currentCycle?.id) {
-                    return {
-                        ...cycle,
-                        stopDate: new Date()
-                    }
-                }
-                return cycle
-            })
-        )
-        setCurrentCycle(null)
+        dispatch({ type: "STOP" })
         document.title = "Pomodoro"
     }
 
+    const done = () => dispatch({ type: "DONE" })
+
     return (
         <CyclesContext.Provider value={{
-            cycles,
-            currentCycle,
+            cycles: cycleStates.cycles,
+            currentCycle: cycleStates.currentCycle,
             done,
             onSubmit,
             handleStopCountdown
